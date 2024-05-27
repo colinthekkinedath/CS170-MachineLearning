f3f86ab1b1cf3d21c7452bdae8a3f86e98bf0a09
colinthekkinedath <colinthekkinedath@gmail.com>
2024-05-27 15:56:20 -0700

    basic program input

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 def main():
+    print("Welcome to Keshav, Colin, Vijay, and David's Feature Selection Algorithm.\n")
+
+    inputfeatures = input('Please enter total number of features:')
+
+    featuresnum = int(inputfeatures)
+
+    inputalgo = input('\nType the number of the algorithm you want to run. \n1. Forward Selection \n2. Backward Elimination \n3. Special Algorithm.\n')
+
+    algonum = int(inputalgo)
+
 
 
 if __name__ == "__main__":
